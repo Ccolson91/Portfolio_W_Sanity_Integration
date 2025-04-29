a659bbfe45c3ddae71b4a8ad23001021a8520d88
Christian Colson <christiancolson91@gmail.com>
2025-04-29 15:49:01 -0500

    skills section added

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,12 +5,13 @@ import type { PageLoad } from "./$types";
 export const load: PageLoad = async () => {
     const workExperience: SanityWorkExperience[] = await sanityClient.fetch('*[_type == "devExperience"] | order(startDate desc)');
     const rawProjects: SanityProject[] = await sanityClient.fetch("*[_type == 'project']");
+    const skills: Skill[] = await sanityClient.fetch(`*[_type == 'skills'][0].skillList`);
 
     const projects = rawProjects.map(processProjectEntries);
 
     // console.log(rawProjects);
 
     return {
-        workExperience, projects
+        workExperience, projects, skills
     };
 }
